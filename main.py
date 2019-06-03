b99fdd36e64eaf8d4ec29df78da6f11a0f9ca335
RageBill <chubillkelvin@gmail.com>
2019-06-04 01:21:40 +0800

    Improve performance.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ current_total_distance = total_length(sequence, all_edges)
 list_length = len(sequence)
 
 for t in range(5000, -1, -1):
-    print(f"T: {t}")
     for i in range(10000):
         new_sequence, new_total_distance = find_neighbour(sequence, all_edges, current_total_distance, list_length)
         if new_total_distance < current_total_distance:
@@ -18,8 +17,10 @@ for t in range(5000, -1, -1):
         elif acceptance(new_total_distance, current_total_distance, t) > random.uniform(0, 1):
             sequence = new_sequence
             current_total_distance = new_total_distance
-    print(f'Best Sequence So Far: {sequence}')
-    print(f'Shortest Distance So Far: {current_total_distance}')
+    if t % 100 is 0:
+        print(f"T: {t}")
+        print(f'Best Sequence So Far: {sequence}')
+        print(f'Shortest Distance So Far: {current_total_distance}')
     if t % 500 is 0:
         plot_graph(data, sequence)
 
