b00ea737e6a4c199e1bc7da0986e1591f37d47cf
Kelvin Chu <chubillkelvin@gmail.com>
2019-06-04 10:22:36 +0800

    Add data.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import read_tsp_data
 from util import *
 import random
 
-data = read_tsp_data.from_file('./data/wi29.txt')
+data = read_tsp_data.from_file('./data/dj38.txt')
 all_edges = eval_all_edges_length(data)
 sequence = random.sample(range(0, len(data)), len(data))
 current_total_distance = total_length(sequence, all_edges)
@@ -28,7 +28,7 @@ for t in range(5000, -1, -1):
     if t % 500 is 0:
         plot_graph(data, sequence)
 
-# T: 0
+# wi29.txt
 # Best Sequence So Far:
 # [1, 5, 9, 10, 14, 18, 17, 16, 20, 21, 22, 28, 27, 25, 19, 24, 26, 23, 15, 13, 12, 11, 7, 8, 6, 2, 3, 4, 0]
 # Shortest Distance So Far: 28042.216388898618
